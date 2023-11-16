819d12ac9a6f55754d76c7ec346968dba542fe89
hoangsang03 <nguyenhoangsangtth2@gmail.com>
2023-11-16 18:59:25 +0700

    Implement_Data_Classes_And_Perform_Migrations Create Entities And DataContext Add Migrations

---
+++ Data/HotelManagementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementAPI.Data
+{
+    public class HotelManagementDbContext : DbContext
+    {
+        public HotelManagementDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
